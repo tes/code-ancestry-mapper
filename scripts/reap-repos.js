eb4556f01711bf905fe165ebbd8dbfaea72f7082
rachelcdavies <rachel.davies@tesglobal.com>
2020-03-06 12:13:18 +0000

    Refactor and prep for next github query.

--- scripts/reap-repos.js
+++ scripts/reap-repos.js
@@ -1,8 +1,9 @@
 import { listPublicReposForOrg } from '../lib/github';
-import { githubOrgName } from '../config.json';
+import { githubOrgName, fetchStatsForRepo } from '../config.json';
 
 (async () => {
   console.log(`Reaping repos for: ${githubOrgName}`);
   const repoNames = await listPublicReposForOrg(githubOrgName);
   console.log(`Repos found : ${repoNames}`);
+  repoNames.forEach((repoName) => fetchStatsForRepo(githubOrgName, repoName));
 })();
