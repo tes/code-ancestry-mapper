db487d12c0dc2508fa5e78a8c8f54afc83a6dd80
Amélie Cornélis <amelie.cornelis@tes.com>
2020-03-06 17:52:05 +0200

    Start with getting list of files for a given repo

--- test/github-reap.test.js
+++ test/github-reap.test.js
@@ -2,7 +2,7 @@ const nock = require('nock');
 const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
-const { listPublicReposForOrg } = require('../lib/github');
+const { listPublicReposForOrg, getFileListForRepo } = require('../lib/github');
 
 describe('Reaping repos', () => {
   it('gets a list of repos for an organisation', async () => {
@@ -31,5 +31,36 @@ describe('Reaping repos', () => {
     const repoNames = await listPublicReposForOrg('tes');
     expect(repoNames).to.include('airbrake-mini-client');
     expect(repoNames).not.to.include('app-house-points');
+  });
+
+  it.skip('gets the code ancestry for a single repo', async () => {
+    nock('https://api.github.com')
+      .post('/graphql')
+      .reply(
+        {
+          data: {
+            repository: {
+              object: {
+                entries: [
+                  {
+                    oid: '1e494d6ae76068964a56874feb3896cdad6afd28',
+                    name: '.gitignore',
+                    type: 'blob',
+                  },
+                  {
+                    oid: 'eb60909865b7afae4b1e8beb9df8b1a84fc2d0f2',
+                    name: 'test',
+                    type: 'tree',
+                  },
+                ],
+              },
+            },
+          },
+        },
+      );
+
+    const files = await getFileListForRepo('tes', 'conflab');
+    expect(files).to.include('.gitignore');
+    expect(files).to.include('test');
   });
 });
