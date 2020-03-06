f5edd44497be15d53f3ae1f1328f271961695ff4
rachelcdavies <rachel.davies@tesglobal.com>
2020-03-06 15:58:23 +0000

    Failing test.

--- test/github-reap.test.js
+++ test/github-reap.test.js
@@ -8,35 +8,36 @@ describe('Reaping repos', () => {
   it('gets a list of repos for an organisation', async () => {
     nock('https://api.github.com')
       .post('/graphql')
-      .reply(200, {
-        data: {
-          organization: {
-            name: 'Tes',
-            repositories: {
-              nodes: [
-                {
-                  name: 'airbrake-mini-client',
-                  isArchived: false,
-                },
-                {
-                  name: 'app-house-points',
-                  isArchived: true,
-                },
-              ],
+      .reply(200,
+        {
+          data: {
+            organization: {
+              name: 'Tes',
+              repositories: {
+                nodes: [
+                  {
+                    name: 'airbrake-mini-client',
+                    isArchived: false,
+                  },
+                  {
+                    name: 'app-house-points',
+                    isArchived: true,
+                  },
+                ],
+              },
             },
           },
-        },
-      });
+        });
 
     const repoNames = await listPublicReposForOrg('tes');
     expect(repoNames).to.include('airbrake-mini-client');
     expect(repoNames).not.to.include('app-house-points');
   });
 
-  it.skip('gets the code ancestry for a single repo', async () => {
+  it('gets the code ancestry for a single repo', async () => {
     nock('https://api.github.com')
       .post('/graphql')
-      .reply(
+      .reply(200,
         {
           data: {
             repository: {
@@ -56,8 +57,7 @@ describe('Reaping repos', () => {
               },
             },
           },
-        },
-      );
+        });
 
     const files = await getFileListForRepo('tes', 'conflab');
     expect(files).to.include('.gitignore');
